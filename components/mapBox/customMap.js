61a911937e39121e75c7bb8a3c5240933228d210
hariscs4997 <hariscs4997@gmail.com>
2022-01-04 19:20:42 +0500

    bug

--- components/mapBox/customMap.js
+++ components/mapBox/customMap.js
@@ -29,9 +29,9 @@ const CustomMap = (value) => {
         zoom={[2]}
         center={
           showIcon ?
-          [value.value?value.value.longitude:74.556022, value.value2?value.value2.latitude:32.483556]
+          [value.value?value.value.longitude:-81.25, value.value2?value.value2.latitude:42.99]
           :
-          [74.556022, 32.483556]
+          [-81.25, 42.99]
         }
         >
   
