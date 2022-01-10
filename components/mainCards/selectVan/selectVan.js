75ac0b86aa84731b261aa5f259e2bd67f0519760
salman6432 <salmantariqofficial@gmail.com>
2022-01-11 00:29:08 +0500

    Login Register Added

--- components/mainCards/selectVan/selectVan.js
+++ components/mainCards/selectVan/selectVan.js
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from "react";
-
-import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
 import Grid from '@mui/material/Grid';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+import React from "react";
+import SignupModal from "./SignupModal";
+import LoginModal from "./LoginModal";
 
 const SelectVan = (props) => {
     const [sVan, setSVan] = React.useState(false);
@@ -36,7 +19,21 @@ const SelectVan = (props) => {
     const [floor, setFloor] = React.useState('Ground');
 
     const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
+    const [openLoginModal, setOpenLoginModal] = React.useState(false);
+    const onSignUp=()=>{
+        setOpen(false)
+    }
+    const onLogin=()=>{
+        setOpenLoginModal(false)
+    }
+    const handleOpenLoginModal=()=>{
+        setOpenLoginModal(true)
+        setOpen(false)
+    }
+    const handleOpen = () => {
+        setOpenLoginModal(false); 
+        setOpen(true);
+    }
     const setActiveVan = (value) => {
         console.log('setActiveVan', value)
         switch (value) {
@@ -187,32 +184,13 @@ const SelectVan = (props) => {
                     key={"Next"}
                     className="darkbutton"
                     sx={{ mb: "16px" }}
-                    onClick={handleOpen}
+                    onClick={handleOpenLoginModal}
                 >
                     Add payment option
                 </Button>  
             </div>
-            <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                open={open}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                timeout: 500,
-                }}
-            >
-                <Fade in={open}>
-                <Box sx={style}>
-                    <Typography id="transition-modal-title" variant="h6" component="h2">
-                    Text in a modal
-                    </Typography>
-                    <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                </Box>
-                </Fade>
-            </Modal>
+            <SignupModal open={open} onSignUp={onSignUp} handleOpenLoginModal={handleOpenLoginModal} />
+            <LoginModal open={openLoginModal} onLogin={onLogin} handleOpenSignUpModal={handleOpen} />
         </>
     );
 };
