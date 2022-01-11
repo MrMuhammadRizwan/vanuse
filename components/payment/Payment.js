20246ba0f4a08704f4e050d17bdd5144cc2f3385
Muhammad Talib Waseem <36582251+MuhammadTalib@users.noreply.github.com>
2022-01-12 00:20:15 +0500

    payment file

---
+++ components/payment/Payment.js
@@ -0,0 +1,25 @@
+@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Payment = () => {
+  return (
+    <>
+      <div className="card-heading mb-31">
+        <h2>Add Items</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+      </div>
+    </>
+  );
+};
+
+export default Payment;
