f2bc903a2340594731b99fbf218bb47f1e0daae8
Muhammad Talib Waseem <36582251+MuhammadTalib@users.noreply.github.com>
2022-01-13 16:15:24 +0500

    payment

--- components/payment/Payment.js
+++ components/payment/Payment.js
@@ -2,19 +2,19 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import Typography from "@mui/material/Typography";
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { getAllCards, postSecret } from "../../pages/api/paymentApi";
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
-const stripePromise = loadStripe("pi_3KGrs1AnCtxHwdDo0Cy1sLPR_secret_2x6YeTGBDktZ8SfWLktj9dsYp");
+const stripePromise = loadStripe("pk_test_DfG4Kda9PiRu28UAJxXIOhC3");
 
-const Payment = () => { 
-const [error, setError] = useState(null);
-   const stripe = useStripe();
-        const elements = useElements();
+const Payment = () => {
+  const [error, setError] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
   // Handle real-time validation errors from the CardElement.
   const handleChange = (event) => {
     if (event.error) {
@@ -23,27 +23,117 @@ const [error, setError] = useState(null);
       setError(null);
     }
   };
-    useEffect(async()=>{
-       let cards = await getAllCards()
-       console.log("cards",cards)
-    },[])
+  useEffect(async () => {
+    let cards = await getAllCards();
+    console.log("cards", cards);
+  }, []);
 
-    const doPayment=async()=>{
-      
-      let secret = await postSecret()
-      console.log("secret",secret)
+  const doPayment = async () => {
+    let secret = await postSecret();
+    console.log("secret", secret);
 
-      const { paymentMethod, error } = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-    });
+    const options = {
+      clientSecret: secret,
+      // Fully customizable with appearance API.
+      appearance: {
+        /*...*/
+      },
+    };
 
-      console.log("paymentMethod",paymentMethod,error)
+    // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 2
+    const elements = stripe.elements(options);
+
+    // Create and mount the Payment Element
+    const paymentElement = elements.create("payment");
+    paymentElement.mount("#payment-element");
+    try {
+      await stripe
+        .confirmPayment({
+          //`Elements` instance that was used to create the Payment Element
+          elements,
+          confirmParams: {
+            return_url:
+              "https://stripe.com/docs/payments/payment-intents/verifying-status",
+          },
+        })
+        .then((res) => {
+          console.log("res", res);
+        })
+        .catch((err) => {
+          console.log("catcc.message", err);
+        });
+    } catch (err) {
+      console.log("caught.message", err);
     }
 
-  return (
- <>         <CardElement id="card-element" onChange={handleChange} />
+    // stripe.retrievePaymentIntent(secret).then(({ paymentIntent }) => {
+    //   console.log("paymentIntent", paymentIntent);
+    //   // Inspect the PaymentIntent `status` to indicate the status of the payment
+    //   // to your customer.
+    //   //
+    //   // Some payment methods will [immediately succeed or fail][0] upon
+    //   // confirmation, while others will first enter a `processing` state.
+    //   //
+    //   // [0]: https://stripe.com/docs/payments/payment-methods#payment-notification
+    //   switch (paymentIntent.status) {
+    //     case "succeeded":
+    //       console.log("Success! Payment received.");
+    //       break;
 
+    //     case "processing":
+    //       console.log(
+    //         "Payment processing. We'll update you when payment is received."
+    //       );
+    //       break;
+
+    //     case "requires_payment_method":
+    //       // Redirect your user back to your payment page to attempt collecting
+    //       // payment again
+    //       console.log("Payment failed. Please try another payment method.");
+    //       break;
+
+    //     default:
+    //       console.log("Something went wrong.");
+    //       break;
+    //   }
+    // });
+
+    // const { error } = await stripe.confirmPayment({
+    //   //`Elements` instance that was used to create the Payment Element
+    //   elements: stripe.elements({
+    //     clientSecret: secret,
+    //   }),
+    //   confirmParams: {
+    //     return_url: "https://my-site.com/order/123/complete",
+    //   },
+    // });
+    // if (error) {
+    //   // This point will only be reached if there is an immediate error when
+    //   // confirming the payment. Show error to your customer (for example, payment
+    //   // details incomplete)
+
+    //   console.log("error.message", error.message);
+    // } else {
+    //   console.log("success", error.message);
+
+    //   // Your customer will be redirected to your `return_url`. For some payment
+    //   // methods like iDEAL, your customer will be redirected to an intermediate
+    //   // site first to authorize the payment, then redirected to the `return_url`.
+    // }
+
+    // const { paymentMethod, error } = await stripe.createPaymentMethod({
+    //   type: "card",
+    //   card: stripe.elements({
+    //     clientSecret: secret,
+    //   }),
+    // });
+
+    // console.log("paymentMethod", paymentMethod, error);
+  };
+
+  return (
+    <>
+      <div id="payment-element"></div>
       <div className="card-heading mb-31">
         <h2>Payment Methods</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
@@ -89,21 +179,21 @@ const [error, setError] = useState(null);
         </Box>
         <Box className="add-btns">
           <Button className="w-100 add-payment">Add Payment Method</Button>
-          <Button className="w-100 next" onClick={doPayment}>Next</Button>
+          <Button className="w-100 next" onClick={doPayment}>
+            Next
+          </Button>
         </Box>
       </div>
-      </>
-
+    </>
   );
 };
 
 const Wrapper = (props) => {
-
   return (
     <Elements stripe={stripePromise}>
       <Payment />
     </Elements>
   );
-}
+};
 
 export default Wrapper;
--- pages/api/paymentApi.js
+++ pages/api/paymentApi.js
@@ -1,23 +1,61 @@
-import axiosHelper from "../../services/axiosHelper"
+import axiosHelper from "../../services/axiosHelper";
 
-export const getAllCards=async (req, res)=> {
+export const getAllCards = async (req, res) => {
   try {
-     const response = await axiosHelper.getRequest('http://127.0.0.1:8000/payment/my_cards')
-    return response.data
+    const response = await axiosHelper.getRequest(
+      "http://127.0.0.1:8000/payment/my_cards"
+    );
+    return response.data;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
- 
-export const postSecret=async (req, res)=> {
+};
+
+export const postSecret = async (req, res) => {
   try {
-     const response = await axiosHelper.postRequest('http://127.0.0.1:8000/payment/secret',{
-        "card_id": "pm_1KGrEJAnCtxHwdDokTzWAZFV"
-    })
-    console.log("response",response)
-    return response.client_secret
+    const response = await axiosHelper.postRequest(
+      "http://127.0.0.1:8000/payment/secret",
+      {
+        card_id: "pm_1KGrEJAnCtxHwdDokTzWAZFV",
+      }
+    );
+    console.log("response", response);
+    return response.client_secret;
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
- 
+};
+export const addCard = async (req, res) => {
+  try {
+    const response = await axiosHelper.postRequest(
+      "http://127.0.0.1:8000/payment/add_card",
+      {
+        number: "4242424242424242",
+        exp_month: 12,
+        exp_year: 2025,
+        cvc: "111",
+      }
+    );
+    console.log("response", response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const removeCard = async (req, res) => {
+  try {
+    const response = await axiosHelper.postRequest(
+      "http://127.0.0.1:8000/payment/remove_card",
+      {
+        number: "4242424242424242",
+        exp_month: 12,
+        exp_year: 2025,
+        cvc: "111",
+      }
+    );
+    console.log("response", response);
+    return response;
+  } catch (err) {
+    console.log(err);
+  }
+};
